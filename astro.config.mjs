9bcf311949cc65b5bf6339672f994ffb6a843848
taketaketaketake <zach@takedetroit.com>
2025-11-04 11:12:12 -0500

    meta uds

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fixmyfurnace.com', // Replace with your actual domain
+  site: 'https://fixmyfurnacedetroit.com',
   vite: {
     plugins: [tailwindcss()],
   },
